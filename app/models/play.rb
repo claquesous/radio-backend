6e150b6fdb669d064ce0fa868e2621d7ec7a2bc4
Geoff Crew <gc97@cornell.edu>
2015-06-27 23:05:32 -0400

    add ratings in response to tweets

--- app/models/play.rb
+++ app/models/play.rb
@@ -1,6 +1,7 @@
 class Play < ActiveRecord::Base
   belongs_to :song
   has_one :artist, through: :song
+  has_many :ratings
   default_scope {order(id: :desc)}
   before_create :tweet_song, if: "Rails.env.production?"
   after_create :send_to_live365, if: "Rails.env.production?"
@@ -12,6 +13,7 @@ class Play < ActiveRecord::Base
 
   def tweet_song
     begin
+      process_replies
       tweet = $twitter_client.update twitter_message
       self.tweet_id = tweet.id
     rescue Twitter::Error
@@ -44,5 +46,31 @@ class Play < ActiveRecord::Base
         return false if play.song == song || (i<40 && play.artist == song.artist)
       end
       true
+    end
+
+    def rate_up(tweet)
+      text = "@#{$twitter_client.user.screen_name} I love this song!"
+      if tweet.full_text == text
+        listener = Listener.find_or_create_by(twitter_handle: tweet.user.screen_name)
+        Play.first.ratings.create(listener: listener, up: true)
+      end
+    end
+
+    def rate_down(tweet)
+      text = "@#{$twitter_client.user.screen_name} I hate this song!"
+      if tweet.full_text == text
+        listener = Listener.find_or_create_by(twitter_handle: tweet.user.screen_name)
+        Play.first.ratings.create(listener: listener, up: false)
+      end
+    end
+
+    def process_replies
+      tweet_id = Play.first.tweet_id
+      $twitter_client.mentions_timeline(since_id: tweet_id).each do |tweet|
+        if tweet.in_reply_to_status_id == tweet_id.to_i
+          rate_up tweet
+          rate_down tweet
+        end
+      end if tweet_id
     end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,13 @@
 class Rating < ActiveRecord::Base
   belongs_to :play
   belongs_to :listener
+  has_one :song, through: :play
+  before_create :update_rating
+
+  def update_rating
+    old = song.rating
+    diff = (100*old-(old*old))/500;
+    song.rating = song.rating.send( up ? :+ : :-, diff)
+    song.save!
+  end
 end
