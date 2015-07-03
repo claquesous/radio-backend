3860af5d03edd72a3c0ad2870ed334752517be20
Geoff Crew <gc97@cornell.edu>
2015-07-03 01:27:30 -0400

    additional exception when sending to live365

--- app/models/play.rb
+++ app/models/play.rb
@@ -24,7 +24,7 @@ class Play < ActiveRecord::Base
   def send_to_live365
     begin
       Live365.send_metadata(song)
-    rescue Curl::Err::HostResolutionError
+    rescue Curl::Err::HostResolutionError, Curl::Err::ConnectionFailedError
       true
     end
   end
