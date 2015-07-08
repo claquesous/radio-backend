23de2dbc278d67bfad80e7fead015729f28b25cf
Geoff Crew <gc97@cornell.edu>
2015-07-07 20:04:26 -0400

    album title should default to 'Single'

--- config/initializers/live_365.rb
+++ config/initializers/live_365.rb
@@ -7,9 +7,9 @@ class Live365
       version: 2,
       title: song.title,
       artist: song.artist.name,
+      album: song.album.try(:title) || "Single",
       seconds: song.time
     }
-    args[:album] = song.album.title if song.album
 
     Curl.post("http://www.live365.com/cgi-bin/add_song.cgi", args) do |curl|
       curl.timeout = 3 # No need to wait for a response
