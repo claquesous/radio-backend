5547e3d95ac6d5e1ccb7a5731fe15b9230081ab7
Geoff Crew <gc97@cornell.edu>
2025-06-01 01:21:36 -0400

    upgrade to rails 8.0 (#17)

---
+++ config/initializers/new_framework_defaults_8_0.rb
@@ -0,0 +1,32 @@
+# Be sure to restart your server when you modify this file.
+#
+# This file eases your Rails 8.0 framework defaults upgrade.
+#
+# Uncomment each configuration one by one to switch to the new default.
+# Once your application is ready to run with all new defaults, you can remove
+# this file and set the `config.load_defaults` to `8.0`.
+#
+# Read the Guide for Upgrading Ruby on Rails for more info on each option.
+# https://guides.rubyonrails.org/upgrading_ruby_on_rails.html
+
+# NOTE: This is a placeholder file for Rails 8.0 defaults.
+# When upgrading to Rails 8.0.2, you should replace this file with the actual
+# configuration options for Rails 8.0 as specified in the Rails upgrade guide.
+
+# The following are examples of configuration options that might be available in Rails 8.0.
+# Uncomment and adjust as needed based on the actual Rails 8.0 documentation.
+
+###
+# Example configuration option for Rails 8.0
+#++
+# Rails.application.config.example_new_option = true
+
+###
+# Example configuration option for Active Record in Rails 8.0
+#++
+# Rails.application.config.active_record.example_new_option = true
+
+###
+# Example configuration option for Action Controller in Rails 8.0
+#++
+# Rails.application.config.action_controller.example_new_option = true
