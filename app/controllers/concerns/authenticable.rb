a51a452a7ee01a06a93111d348bf53522406a60e
Geoff Crew <gc97@cornell.edu>
2025-06-02 19:56:49 -0400

    remove extra code

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -3,13 +3,30 @@ module Authenticable
 
   included do
     before_action :authenticate_request
-    attr_reader :current_user
   end
 
   private
 
   def authenticate_request
-    @current_user = AuthorizeApiRequest.call(request.headers).result
+    @current_user = nil
+
+    token = http_auth_header
+    begin
+      decoded_token = decode_token(token) if token
+      @current_user = User.find(decoded_token[:user_id]) if decoded_token
+    rescue JWT::DecodeError, ActiveRecord::RecordNotFound => e
+      # Authentication failed, @current_user remains nil
+    end
+
     render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+  end
+
+  def http_auth_header
+    request.headers['Authorization'].present? ? request.headers['Authorization'].split(' ').last : nil
+  end
+
+  def decode_token(token)
+    body = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
+    HashWithIndifferentAccess.new body
   end
 end
